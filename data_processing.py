c05d6387ae730512e62fccac300a4440d5859054
hyyc116 <hyyc116@gmail.com>
2017-09-15 00:21:32 -0400

    add

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,22 @@
 #coding:utf8
 
+def retrive_all_unique_names():
+    query_op = dbop()
+    sql = 'select first_name,middle_name,last_name from  proquest_article_authorname'
+    cursor  = query_op.query_database(sql)
+
+    name_set = []
+    for row in cursor:
+        name  = ','.join(row)
+        name_set.append(name)
+
+    print len(name_set)
+
+    name_list =list(set(name_set))
+
+    print len(name_list)
+
+    open('unique_names.txt','w').write('\n'.join(name_list))
+
+if __name__ == '__main__':
+     retrive_all_unique_names() 
