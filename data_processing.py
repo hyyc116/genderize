9003236354c601c71e3e98197563488cc8a5d9bd
hyyc116 <hyyc116@gmail.com>
2017-09-15 00:30:48 -0400

    add

--- data_processing.py
+++ data_processing.py
@@ -18,7 +18,7 @@ def retrive_all_unique_names():
     print 'unique article authors:',len(name_list)
 
     ## add advisors' name
-    sql = 'select adviser_name form proquest_article_adviser'
+    sql = 'select adviser_name from proquest_article_adviser'
 
     cursor = query_op.query_database(sql)
     for row in cursor:
@@ -36,7 +36,7 @@ def retrive_all_unique_names():
 
     name_list = list(set(name_list))
 
-    print 'unique add advisors',len(name_list)
+    print 'unique add advisors]',len(name_list)
 
 
     open('unique_names.txt','w').write('\n'.join(name_list))
