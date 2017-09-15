49e0f18daaf5a58aaa480a2953ca5ecc7708bca4
hyyc116 <hyyc116@gmail.com>
2017-09-15 12:43:16 -0400

    add

--- data_processing.py
+++ data_processing.py
@@ -32,7 +32,7 @@ def retrive_all_unique_names():
         elif len(names)>3:
             if ',' in row[0]:
                 names = row[0].split(',')[0].split()
-                name_list.append([','.join(names[0],' '.join(names[1:-1],names[-1]))])
+                name_list.append(','.join([names[0],' '.join(names[1:-1]),names[-1]]))
 
         elif len(names)==1:
             name_list.append(','.join(names[0],'',''))
