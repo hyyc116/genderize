a244279c3ebbd3d7f6eb6fd11375e72c1d730422
hyyc116 <hyyc116@gmail.com>
2017-09-21 00:30:20 -0400

    add

--- data_processing.py
+++ data_processing.py
@@ -13,8 +13,8 @@ def retrive_proquest_unique_names():
 
     name_set = []
     for row in cursor:
-        name  = ','.join(row)
-        name_set.append(name)
+        # name  = ','.join(row)
+        name_set.append(row[0])
 
     print 'article authors:',len(name_set)
 
@@ -30,17 +30,7 @@ def retrive_proquest_unique_names():
     cursor = query_op.query_database(sql)
     for row in cursor:
         names = row[0].split()
-        if len(names)==3:
-            name_list.append(','.join(names))
-        elif len(names)==2:
-            name_list.append(','.join([names[0],'',names[1]]))
-        elif len(names)>3:
-            if ',' in row[0]:
-                names = row[0].split(',')[0].split()
-                name_list.append(','.join([ names[0],' '.join(names[1:-1]),names[-1]]))
-
-        elif len(names)==1:
-            name_list.append(','.join([names[0],'','']))
+        name_list.append(names[0])
 
     print 'add  advisors:',len(name_list)
 
@@ -76,11 +66,8 @@ def retirve_aminer_unique_names(aminer_names):
 
         last,first = splits
 
-        mid =  author['middlename']
-        if mid is None or mid=='null':
-            mid = ""
 
-        name = ','.join([first,mid,last])
+        name = first
         name_List.append(name.encode('utf-8', 'ignore'))
 
     print len(name_List)
@@ -93,5 +80,6 @@ def retirve_aminer_unique_names(aminer_names):
 
 
 if __name__ == '__main__':
+    retrive_proquest_unique_names()
     retirve_aminer_unique_names(sys.argv[1])
     
