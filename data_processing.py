30833d2e24c374d668ddc972ee0b58272ce32b32
hyyc116 <hyyc116@gmail.com>
2017-09-28 22:32:17 -0400

    add

--- data_processing.py
+++ data_processing.py
@@ -4,6 +4,7 @@ import json
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+from collections import Counter
 
 def retrive_proquest_unique_names():
     query_op = dbop()
@@ -112,8 +113,62 @@ def retirve_aminer_unique_names(aminer_names,other_names):
 
     open('all_names.txt','w').write('\n'.join(all_names))
 
+def percent_of_request(genderpath):
+    name_dict={}
+    for line in open(genderpath):
+        line = line.strip()
+        splits = line.split('\t')
+        if splits[0]=='YES':
+            g = json.loads(splits[-1])['gender']
+            name_dict[splits[1]] = g
+
+
+    query_op = dbop()
+    query_op.connect_aws()
+    sql = 'select first_name,middle_name,last_name from  proquest_article_authorname'
+    cursor  = query_op.query_database(sql)
+
+    name_set = []
+    for row in cursor:
+        # name  = ','.join(row)
+        name_set.append(row[0].lower())
+
+    print 'article authors:',len(name_set)
+
+    ## add advisors' name
+    sql = 'select adviser_name from proquest_article_adviser'
+
+    '''这里有很多长度大于四的如何处理？'''
+
+    cursor = query_op.query_database(sql)
+    for row in cursor:
+        names = row[0].split()
+        name_list.append(names[0].lower())
+
+    print 'add  advisors:',len(name_list)
+
+    genders = []
+    for name in name_list:
+        if name_dict.get(name,-1)!=-1:
+            genders.append(name_dict[name])
+        else:
+            if '-' in name:
+                splits = name.split('-')
+                g = ('-1',0)
+                for s in splits:
+                    if name_dict.get(s,-1)!=-1 and name_dict[s]>g[1]:
+                        g = (s,name_dict[s])
+
+                if g[0]!='-1':
+                    genders.append(g[0])
+
+    gc = Counter(genders)
+    print 'total:',len(name_list),'with gender:',len(genders),'percent = {:.2f}'.format(len(genders)/float(len(name_list)))
+    for g in gc.keys():
+        print 'gender:',g,'num:'mgc[g]
 
 if __name__ == '__main__':
-    anames = retrive_proquest_unique_names()
-    retirve_aminer_unique_names(sys.argv[1],anames)
+    # anames = retrive_proquest_unique_names()
+    # retirve_aminer_unique_names(sys.argv[1],anames)
+    percent_of_request(sys.argv[1])
     
