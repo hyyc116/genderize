bee97c72d76b9234542169b329d815852d371caa
hyyc116 <hyyc116@gmail.com>
2017-10-06 01:04:58 -0400

    add

--- gender_dis.py
+++ gender_dis.py
@@ -36,6 +36,7 @@ def get_gender_data(genderpath):
     paper_count =0 
     for row in cursor:
         ditm_id = row[0]
+        print row[0],row[1]
         adviser_first_name = row[1].split()[0].lower()
         author_first_name = row[2].split(',')[1].lower()
         degree = row[3]
