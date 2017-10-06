4e639e0d23037d4a2e9380d427c7d4c315bc8a12
hyyc116 <hyyc116@gmail.com>
2017-10-06 10:36:04 -0400

    add

---
+++ gender_data.py
@@ -0,0 +1,79 @@
+#coding:utf8
+from db_util import *
+import json
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+from collections import Counter
+from collections import defaultdict
+
+def gen_stats(filepath):
+
+    ## generate gender with degree
+    lines = []
+
+    for line in open(filepath):
+        line = line.strip()
+        splits = line.split('====')
+        
+
+        ad_gender = splits[1]
+        au_gender = splits[2]
+
+        gender_type='MM'
+
+        if ad_gender=='male':
+            if au_gender=='male':
+                gender_type='MM'
+            elif au_gender=='female':
+                gender_type='MF'
+        elif ad_gender=='female':
+            if au_gender=='male':
+                gender_type='FM'
+            elif au_gender=='female':
+                gender_type='FF'
+
+        degree = splits[3]
+        school = splits[4]
+        year = splits[5]
+        cats = splits[6]
+        country = splits[7]
+
+        lines.append([gender_type,degree,school,year,cats,country])
+
+    print generate_dict(lines,1)
+
+
+
+def generate_dict(lines,index):
+    attr_dict = defaultdict(list)
+    for line in lines:
+        gender_type = line[0]
+        attr = line[index]
+
+        attr_dict[attr].append(gender_type)
+
+
+    result_dict = {}
+    ## 根据属性对应的数量进行排序
+    for k,v in sorted(attr_dict.items(),key=lambda x:len(x[1]),reverse=True)[10]:
+
+        result_dict['State'] = k
+        result_dict['freq'] = Counter(v)
+
+
+    return result_dict
+
+
+if __name__ == '__main__':
+    gen_stats(sys.argv[1])
+
+
+
+
+
+
+
+
+
+
